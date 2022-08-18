8d1fd6b48355f56961dc2704505b2adf07b8385e
jigyasa <jigyasabajaj93@gmail.com>
2022-08-18 19:34:18 +0530

    changes

--- Scenario2.java
+++ Scenario2.java
@@ -17,28 +17,46 @@ public class Scenario2 {
 		System.setProperty("webdriver.chrome.driver","C:/Users/LENOVO/Downloads/chromedriver_win32/chromedriver.exe");	
 	    WebDriver driver = new ChromeDriver();
         driver.get(" https://in.yahoo.com");
-        driver.manage().window().maximize();
-        JavascriptExecutor js = (JavascriptExecutor) driver;
+        //driver.manage().window().maximize();
+        //JavascriptExecutor js = (JavascriptExecutor) driver;
         String text = driver.findElement(By.cssSelector("h4[class=\"title d-i fz-s fw-xl s-label\"]")).getText();
         Assert.assertEquals(text, "Trending searches");
         //int count = driver.findElements(By.tagName("a")).size();
         //System.out.println(count);
       WebElement trendingsearch =   driver.findElement(By.xpath("(//li[@class='last'])[2]"));
       List <WebElement> links = trendingsearch.findElements(By.tagName("a"));
-      System.out.println(links.size());
+      System.out.println("Total no. of links under Trending search are "+ links.size());
      // Iterator<WebElement> it = links.iterator();
-     
+      String[] linklist = new String[links.size()];
+      System.out.println("List of links Available: ");  
+      // print all the links from webpage 
+      for(int i=0;i<links.size();i++)
+      {
+      linklist[i] = links.get(i).getText();
+      System.out.println(links.get(i).getText());
+      } 
+      // navigate to each Link on the webpage
+      for(int i=0;i<links.size();i++)
+      {
+    	  linklist[i]= links.get(i).getAttribute("href");
+      driver.navigate().to(linklist[i]);
+      driver.navigate().back();
+      Thread.sleep(3000);
+      }
     
-     for(int i=0; i<links.size() ; i++)
+     
+    /*
+    for(int i=0; i<links.size() ; i++)
     
       {
-    	 if(!(links.get(i).getText().isEmpty()))
-         {
-    	 links.get(i).click();
+    	 
+    	// if(!(links.get(i).getText().isEmpty()))
+         //{
+    
     	 driver.navigate().back(); 
     	 
-    	 js.executeScript("window.scrollBy (0,500)");
-    	 links = trendingsearch.findElements(By.tagName("a"));
+    	 //js.executeScript("window.scrollBy (0,500)");
+    	// links = trendingsearch.findElements(By.tagName("a"));
     	 
     	// it.next();
     	 //System.out.println(links.get(i).getText());
@@ -49,8 +67,8 @@ public class Scenario2 {
       }
       //driver.findElement(By.cssSelector("a[href*=\"&p=coronavirus\"]")).click();
       //System.out.println(driver.findElement(By.cssSelector("div[id=\"doc\"]")).getText());
-      
+      */
       }
 	}
 
-}
+
--- Scenario4.java
+++ Scenario4.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class Scenario4 {
 
@@ -10,8 +11,15 @@ public class Scenario4 {
          WebDriver driver = new ChromeDriver();
          driver.manage().window().maximize();
          driver.get("https://www.myntra.com/");
-         driver.findElement(By.xpath("(//div[@class='desktop-navContent'])[2]")).click();
-         driver.findElement(By.linkText("Kurtas & Suits")).click();
+         Actions a = new Actions(driver);
+         a.moveToElement(driver.findElement(By.xpath("//a[@href=\"/shop/women\"]"))).build().perform();
+         //driver.findElement(By.xpath("(//div[@class='desktop-navContent'])[2]")).click();
+       a.moveToElement(driver.findElement(By.linkText("Kurtas & Suits"))).click().build().perform();
+	driver.findElement(By.cssSelector("div[class=\"sort-sortBy\"]")).click();
+	a.moveToElement(driver.findElement(By.cssSelector("label[class=\"sort-label\"]"))).click().build().perform();
+	driver.findElement(By.cssSelector("div[class=\"common-checkboxIndicator\"]")).click();
+	driver.findElement(By.xpath("//body/div[@id='mountRoot']/div[1]/div[1]/main[1]/div[3]/div[1]/section[1]/div[1]/div[3]/ul[1]/li[1]/label[1]/div[1]")).click();
+	driver.findElement(By.cssSelector("div[class=\"common-radioIndicator\"]")).click();
 	}
 
 }
