c99adf12bbc59289fce1bc2a477eedb20a3fef27
jigyasa <jigyasabajaj93@gmail.com>
2022-08-20 18:44:17 +0530

    Automation Assignment

--- Scenario3.java
+++ Scenario3.java
@@ -1,15 +1,16 @@
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 
-public class Scenario2 {
+public class Scenario3 {
 
-	public static void main(String[] args) throws InterruptedException 
+	public static void main(String[] args)
 	{
 		System.setProperty("webdriver.chrome.driver","C:/Users/LENOVO/Downloads/chromedriver_win32/chromedriver.exe");	
 	    WebDriver driver = new ChromeDriver();
@@ -17,9 +18,13 @@ public class Scenario2 {
         driver.get(" https://in.yahoo.com");
       
         String text = driver.findElement(By.cssSelector("h4[class=\"title d-i fz-s fw-xl s-label\"]")).getText();
-        Assert.assertEquals(text, "Trending searches");
+      // String text = driver.findElement(By.xpath("//h4[@class=\"title d-i fz-s fw-xl s-label\"]")).getText();
+     //  String text = driver.findElement((By.xpath("/html[1]/body[1]/div[2]/div[1]/ol[2]/li[1]/div[1]/div[1]/ul[1]/li[1]/div[1]/h4[1]"))).getText(); 
+       Assert.assertEquals(text, "Trending searches");
         
       WebElement trendingsearch =   driver.findElement(By.xpath("(//li[@class='last'])[2]"));
+    // WebElement trendingsearch =   driver.findElement(By.xpath("//ol[@class='reg searchBottom']//li[@class='last']"));
+    //  WebElement trendingsearch =   driver.findElement(By.xpath("/html[1]/body[1]/div[2]/div[1]/ol[2]/li[1]"));
       List <WebElement> links = trendingsearch.findElements(By.tagName("a"));
       
       //Get the count of link under trendingsearch
@@ -42,12 +47,15 @@ public class Scenario2 {
     	  linklist[i]= links.get(i).getAttribute("href");
           driver.navigate().to(linklist[i]);
           driver.navigate().back();
-          //driver.navigate().refresh();
-          //Thread.sleep(3000);
       }
       
       driver.findElement(By.cssSelector("a[href*=\"&p=coronavirus\"]")).click();
+     // driver.findElement(By.linkText("COVID-19 Updates")).click();
+    //driver.findElement(By.className("thmb")).click();
+    //  driver.findElement(By.xpath("//a[@aria-label='COVID-19 Updates']")).click();
       driver.findElement(By.cssSelector("span[title=\"Coronavirus Highlights: India Reports 15,754 New Covid Cases, 39 Deaths In 24 Hours\"]")).click();
+      //driver.findElement(By.xpath("//span[@title=\"Coronavirus Highlights: India Reports 15,754 New Covid Cases, 39 Deaths In 24 Hours\"]")).click();
+    // driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/ol[1]/li[2]/div[1]/ul[1]/li[1]/a[1]/div[2]/h4[1]/span[1]")).click();
       Set<String> window =  driver.getWindowHandles();
       Iterator<String> it =  window.iterator();
       String parentid = it.next();
@@ -55,7 +63,8 @@ public class Scenario2 {
       driver.switchTo().window(childid);
       System.out.println("All content of cases overview is  " + driver.findElement(By.id("ins_storybody")).getText());
       driver.switchTo().window(parentid);
-      driver.quit();
-	}}
-	
+      driver.quit();	
 
+	}
+
+}
--- Scenario4.java
+++ Scenario4.java
@@ -1,7 +1,11 @@
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Scenario4 {
 
@@ -10,16 +14,19 @@ public class Scenario4 {
 		System.setProperty("webdriver.chrome.driver","C:/Users/LENOVO/Downloads/chromedriver_win32/chromedriver.exe");		// TODO Auto-generated method stub
          WebDriver driver = new ChromeDriver();
          driver.manage().window().maximize();
+         WebDriverWait w = new WebDriverWait(driver,Duration.ofSeconds(15));
          driver.get("https://www.myntra.com/");
          Actions a = new Actions(driver);
          a.moveToElement(driver.findElement(By.xpath("//a[@href=\"/shop/women\"]"))).build().perform();
          //driver.findElement(By.xpath("(//div[@class='desktop-navContent'])[2]")).click();
        a.moveToElement(driver.findElement(By.linkText("Kurtas & Suits"))).click().build().perform();
-	driver.findElement(By.cssSelector("div[class=\"sort-sortBy\"]")).click();
-	a.moveToElement(driver.findElement(By.cssSelector("label[class=\"sort-label\"]"))).click().build().perform();
-	driver.findElement(By.cssSelector("div[class=\"common-checkboxIndicator\"]")).click();
-	driver.findElement(By.xpath("//body/div[@id='mountRoot']/div[1]/div[1]/main[1]/div[3]/div[1]/section[1]/div[1]/div[3]/ul[1]/li[1]/label[1]/div[1]")).click();
-	driver.findElement(By.cssSelector("div[class=\"common-radioIndicator\"]")).click();
+       w.until(ExpectedConditions.alertIsPresent());
+       driver.switchTo().alert().accept();
+	a.moveToElement(driver.findElement(By.cssSelector("div[class=\"sort-sortBy\"]"))).build().perform();
+	a.moveToElement(driver.findElement(By.cssSelector("//ul[@class=\"sort-list\"][2]"))).click().build().perform();
+	//driver.findElement(By.cssSelector("div[class=\"common-checkboxIndicator\"]")).click();
+	//driver.findElement(By.xpath("//body/div[@id='mountRoot']/div[1]/div[1]/main[1]/div[3]/div[1]/section[1]/div[1]/div[3]/ul[1]/li[1]/label[1]/div[1]")).click();
+	//driver.findElement(By.cssSelector("div[class=\"common-radioIndicator\"]")).click();
 	}
 
 }
