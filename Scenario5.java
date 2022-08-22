b3354d1429fa0d076f1e585e38b54dae4d9be505
jbajaj19 <110085660+jbajaj19@users.noreply.github.com>
2022-08-22 11:47:13 +0530

    Automation assignment

--- Scenario5.java
+++ Scenario5.java
@@ -5,18 +5,24 @@ import java.util.Set;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
-public class Scenario4 {
+public class Scenario5 {
 
 	public static void main(String[] args)
 	{
-		System.setProperty("webdriver.chrome.driver","C:/Users/LENOVO/Downloads/chromedriver_win32/chromedriver.exe");		// TODO Auto-generated method stub
-         WebDriver driver = new ChromeDriver();
-         driver.manage().window().maximize();
+		//System.setProperty("webdriver.gecko.driver","C:/Users/LENOVO/Downloads/geckodriver-v0.31.0-win64/geckodriver.exe");
+		//WebDriver driver = new FirefoxDriver();
+		System.setProperty("webdriver.edge.driver","C:/Users/LENOVO/Downloads/edgedriver_win64/msedgedriver.exe");
+		WebDriver driver = new EdgeDriver();
+		//System.setProperty("webdriver.chrome.driver","C:/Users/LENOVO/Downloads/chromedriver_win32/chromedriver.exe");		// TODO Auto-generated method stub
+        //WebDriver driver = new ChromeDriver();
+		 driver.manage().window().maximize();
          WebDriverWait w = new WebDriverWait(driver,Duration.ofSeconds(15));
          driver.get("https://www.myntra.com/");
          Actions a = new Actions(driver);
@@ -27,9 +33,9 @@ public class Scenario4 {
        driver.switchTo().alert().accept();
 	a.moveToElement(driver.findElement(By.cssSelector("div[class=\"sort-sortBy\"]"))).build().perform();
 	a.moveToElement(driver.findElement(By.cssSelector("//ul[@class=\"sort-list\"][2]"))).click().build().perform();
-	//driver.findElement(By.xpath("(//div[@class='common-checkboxIndicator'])[1]")).click();
-	//driver.findElement(By.xpath("(//div[@class='common-checkboxIndicator'])[3]")).click();
-	//driver.findElement(By.xpath("(//div[@class='common-checkboxIndicator'])[10]")).click();
+	driver.findElement(By.xpath("(//div[@class='common-checkboxIndicator'])[1]")).click();
+	driver.findElement(By.xpath("(//div[@class='common-checkboxIndicator'])[3]")).click();
+	driver.findElement(By.xpath("(//div[@class='common-checkboxIndicator'])[10]")).click();
 	driver.findElement(By.xpath("//div[@class=\"common-radioIndicator\"][1]")).click();
 	driver.findElement(By.xpath("(//img[@title='7Threads Women Magenta & dark purple Yoke Design Kurta'])[1]")).click();
 	 Set<String> window =  driver.getWindowHandles();
@@ -49,5 +55,7 @@ public class Scenario4 {
 	driver.findElement(By.className("submitBottomOption")).click();
 	driver.findElement(By.xpath("//span[contains(text(),'Password')]")).click();
 	driver.findElement(By.cssSelector("input[type=\"password\"]")).click();*/
+
 	}
+
 }
