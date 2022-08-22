8e3c65a22639811e1ebc4a815b33e3d25968e542
jbajaj19 <110085660+jbajaj19@users.noreply.github.com>
2022-08-22 17:47:39 +0530

    Automation assignment

--- Scenario5.java
+++ Scenario5.java
@@ -1,15 +1,11 @@
-import java.time.Duration;
 import java.util.Iterator;
 import java.util.Set;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 public class Scenario5 {
@@ -22,27 +18,23 @@ public class Scenario5 {
 		WebDriver driver = new EdgeDriver();
 		//System.setProperty("webdriver.chrome.driver","C:/Users/LENOVO/Downloads/chromedriver_win32/chromedriver.exe");		// TODO Auto-generated method stub
         //WebDriver driver = new ChromeDriver();
-		 driver.manage().window().maximize();
-         WebDriverWait w = new WebDriverWait(driver,Duration.ofSeconds(15));
-         driver.get("https://www.myntra.com/");
-         Actions a = new Actions(driver);
-         a.moveToElement(driver.findElement(By.xpath("//a[@href=\"/shop/women\"]"))).build().perform();
-         //driver.findElement(By.xpath("(//div[@class='desktop-navContent'])[2]")).click();
-       a.moveToElement(driver.findElement(By.linkText("Kurtas & Suits"))).click().build().perform();
-       w.until(ExpectedConditions.alertIsPresent());
-       driver.switchTo().alert().accept();
+		driver.manage().window().maximize();
+        driver.get("https://www.myntra.com/");
+        Actions a = new Actions(driver);
+        a.moveToElement(driver.findElement(By.xpath("//a[@href=\"/shop/women\"]"))).build().perform();
+      a.moveToElement(driver.findElement(By.linkText("Kurtas & Suits"))).click().build().perform(); 
 	a.moveToElement(driver.findElement(By.cssSelector("div[class=\"sort-sortBy\"]"))).build().perform();
-	a.moveToElement(driver.findElement(By.cssSelector("//ul[@class=\"sort-list\"][2]"))).click().build().perform();
-	driver.findElement(By.xpath("(//div[@class='common-checkboxIndicator'])[1]")).click();
-	driver.findElement(By.xpath("(//div[@class='common-checkboxIndicator'])[3]")).click();
-	driver.findElement(By.xpath("(//div[@class='common-checkboxIndicator'])[10]")).click();
-	driver.findElement(By.xpath("//div[@class=\"common-radioIndicator\"][1]")).click();
+	a.moveToElement(driver.findElement(By.xpath("//div[@class='horizontal-filters-sortContainer']//li[2]//label[1]"))).click().build().perform();
+	driver.findElement(By.xpath("//body[1]/div[2]/div[1]/div[1]/main[1]/div[3]/div[1]/section[1]/div[1]/div[2]/ul[1]/li[1]/label[1]/div[1]")).click();
+	driver.findElement(By.xpath("//body[1]/div[2]/div[1]/div[1]/main[1]/div[3]/div[1]/section[1]/div[1]/div[3]/ul[1]/li[1]/label[1]/div[1]")).click();
+	driver.findElement(By.xpath("//body[1]/div[2]/div[1]/div[1]/main[1]/div[3]/div[1]/section[1]/div[1]/div[3]/ul[1]/li[2]/label[1]/div[1]")).click();
+	driver.findElement(By.xpath("(//label[normalize-space()='50% and above'])[1]")).click();
 	driver.findElement(By.xpath("(//img[@title='7Threads Women Magenta & dark purple Yoke Design Kurta'])[1]")).click();
 	 Set<String> window =  driver.getWindowHandles();
-     Iterator<String> it =  window.iterator();
-     String parentid = it.next();
-     String childid = it.next();
-     driver.switchTo().window(childid);
+    Iterator<String> it =  window.iterator();
+    String parentid = it.next();
+    String childid = it.next();
+    driver.switchTo().window(childid);
 	driver.findElement(By.cssSelector("button[class=\"size-buttons-size-button size-buttons-size-button-default\"]")).click();
 	driver.findElement(By.xpath("//body[1]/div[2]/div[1]/div[1]/div[1]/main[1]/div[2]/div[2]/div[4]/div[1]/div[1]")).click();
 	driver.findElement(By.cssSelector("span[class=\"myntraweb-sprite pdp-whiteRightArrow sprites-whiteRightArrow\"]")).click();
@@ -50,6 +42,7 @@ public class Scenario5 {
 	Assert.assertEquals(text, "Women Magenta & dark purple Yoke Design Kurta");
 	driver.findElement(By.cssSelector("button[class=\"inlinebuttonV2-base-actionButton bulkActionStrip-desktopBulkWishlist\"]")).click();
 	driver.findElement(By.cssSelector("button[class=\"inlinebuttonV2-base-actionButton bulkActionStrip-waterMelon\"]")).click();
+	
 	/*driver.findElement(By.cssSelector("input[type =\"tel\"]")).click();
 	driver.findElement(By.cssSelector("input[type =\"tel\"]")).sendKeys("8104866781");
 	driver.findElement(By.className("submitBottomOption")).click();
